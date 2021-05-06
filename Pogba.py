362cc1e609be1590dae3bc140c72b3721d6f2754
nsi-lfv-duc <79856190+nsi-lfv-duc@users.noreply.github.com>
2021-05-06 14:55:34 +0200

    Update Pogba.py

--- Pogba.py
+++ Pogba.py
@@ -1,34 +1,49 @@
-from PIL import Image
-##from IPython.display import display
-#import urllib.request
+from __future__ import division
+from PIL import Image 
+import math
+import os
 
-# ouvrir une image hébergée sur internet
-im = Image.open("pog.jpg")
+img = Image.open("pog.jpg")
 
-# informations sur l'image
-print(im.format, im.size, im.mode)
+width, height = img.size
+new_img= Image.new("RGB",(height,width),(255,255,255))
 
-# taille de l'image
-width, height = im.size
-im_new = Image.new("RGB", (width, height), (255,255,255))
-# valeurs du pixel de coordonnées x, y (l'origine (0, 0) est en haut à gauche)
-def pogba():   
-        for x in range(width):
-            for y in range(0,height):
+def long_slice(image_path, out_name, outdir, slice_size):
+    """slice an image into parts slice_size tall"""
+    upper = 0
+    left = 0
+    slices = int(math.ceil(height/slice_size))
 
-                pixel = im.getpixel((x, y))
-                p_rouge = pixel[0]
-                p_vert =  pixel[1]
-                p_bleu =  pixel[2]
-                im_new.putpixel((x,y),(p_rouge, p_vert, p_bleu))
-    
-        for x in range(0,height):
-            for y in range(0,width):
-                a= (x, y, x+width, y+height)
-                b= im.crop(a)
+    count = 1
+    for slice in range(slices):
+        #if we are at the end, set the lower bound to be the bottom of the image
+        if count == slices:
+            lower = height
+        else:
+            lower = int(count * slice_size)  
 
-# sauvegarder la nouvelle image et la montrer             
-        im_new.save("outcome.png")
-        im_new.show()
+        bbox = (left, upper, width, lower)
+        new_img = img.crop(bbox)
+        upper += slice_size
+        #save the slice
+        new_img.save(os.path.join(outdir,out_name + "_" + str(count)+".png"))
+        count +=1
+        img1= Image.open("pogba_1.png")
+        img2= Image.open("pogba_2.png")
+        img3= Image.open("pogba_3.png")
+        img4= Image.open("pogba_4.png")
+        new_img= Image.new('RGB', (img1.width, img1.height + img2.height + img3.height + img4.height),(255,255,255))
+        new_img.paste(img1, (0, 0))
+        new_img.paste(img2, (0, 100))
+        new_img.paste(img3, (0, 210))
+        new_img.paste(img4, (0, 320))
+        new_img.save("zzz.jpg")
+        new_img.show()
+        time.show(1)
+       
+        
+        
+if __name__ == '__main__':
+    long_slice("pog.jpg","pogba", os.getcwd(), 90)
 
-pogba()
+
